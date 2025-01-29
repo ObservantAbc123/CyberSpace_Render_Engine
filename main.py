69d4ad58f83bc79534578623c67e9e680bad963b
ObservantAbc123 <hunterleavitt@gmail.com>
2025-01-28 23:19:28 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,74 @@
+
+import scapy
+from scapy.all import sniff
+import pygame
+import scapy.sendrecv
+
+# 1. create a backend using scapy and stdout
+#    style text output.
+
+# 2. link that to a pygame representation of
+#    a graph showing the network traffic/use.
+     
+
+DEBUG_MODE = 1
+###
+packets_captured = 0
+captured_packets = dict()
+###
+link_layer_protocol = dict()
+network_layer_protocol = dict()
+transport_layer_protocol = dict()
+application_layer_protocol = dict()
+sending_ip_address = dict()
+receiving_ip_address = dict()
+flags = dict()
+
+
+
+def packet_callback(captured_packet):
+    if not DEBUG_MODE and captured_packet is not None:
+        captured_packets[packets_captured] = captured_packet
+        packets_captured += 1
+    else:
+        print(captured_packet.summary())
+
+
+
+def execute_packet_capture():
+    capture_time = input("--enter the number of seconds for capture duration--\r\n")
+    print("--beginning capture--\r\n")
+
+    packet_capture = sniff(timeout=int(capture_time), prn=lambda packets:packet_callback(packets))
+    
+    print("--capture ended--\r\n")
+
+
+def analyze_packets(packet_capture):
+    # is_not_none = True
+    # marker = 0
+
+    for marker in packet_capture.keys:
+            packet_summary = packet_capture[marker].summary()
+            split_capture = str.split(packet_summary, '>')
+            split_protocols_sender = split_capture[0]
+            split_receiver_port_flags = split_capture[1]
+
+            # link_layer_protocol[marker] = 
+
+    # while is_not_none:
+    #     if not packet_capture[marker] == None: 
+    #         packet_summary = packet_capture[marker].summary()
+    #         split_capture = str.split(packet_summary, ':')
+    #         link_layer_protocol[]
+    #     else:
+    #         is_not_none = False
+    #     marker+=1
+
+
+if __name__ == '__main__':
+    execute_packet_capture()
+    if not DEBUG_MODE:
+        print("--%d packets captured; storing analyzing and packet attributes--", packets_captured) 
+        analyze_packets(captured_packets)
+
